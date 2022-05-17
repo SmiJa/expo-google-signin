9faa9d4f06909a3f8db8825a70a13c13b5dd52ce
Jason Smith <jasons@alphaworks.tech>
2022-05-17 16:37:58 -0700

    fix errors

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from "expo-auth-session/providers/google"
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    webClientId: "931819812753-3oqkhfhmmeifcu5in3js00m0h7plnqaj.apps.googleusercontent.com",
+    expoClientId: "931819812753-ca3g8djd51p1kkuh9uddv3eua9pfjgqt.apps.googleusercontent.com",
+    androidClientId: "931819812753-ca3g8djd51p1kkuh9uddv3eua9pfjgqt.apps.googleusercontent.com",
+    iosClientId: "931819812753-ca3g8djd51p1kkuh9uddv3eua9pfjgqt.apps.googleusercontent.com"
+  });
+
+  const [loggedIn, setLoggedIn] = useState("");
+
+  useEffect(
+    () => {
+      if(response?.type === 'success') {
+        const { authentication, type } = response;
+        setLoggedIn(type)
+      }
+    }, [response]
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        disabled={!request}
+        title={"Login"}
+        onPress={() => {
+          promptAsync();
+        }}
+      />
+      <Text>
+        {loggedIn === "success" ? "Logged In" : "Logged Out"}
+      </Text>
     </View>
   );
 }
